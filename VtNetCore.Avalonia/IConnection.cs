07563c37993d48b1286e6bb5070b6e3e8747e83d
Ii Daisuke <dai@e-frontier.systems>
2025-03-08 09:24:20 +0900

    VtNetCoreプロジェクトとVtNetCore.Avaloniaプロジェクトを追加しました

---
+++ VtNetCore.Avalonia/IConnection.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace VtNetCore.Avalonia
+{
+    public class DataReceivedEventArgs : EventArgs, IEquatable<DataReceivedEventArgs>
+    {
+        public byte[] Data { get; set; }
+
+        public bool Equals(DataReceivedEventArgs other)
+        {
+            return ReferenceEquals(this, other);
+        }
+    }
+
+    public interface IConnection
+    {
+        bool IsConnected { get; }
+
+        event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        event EventHandler<EventArgs> Closed;
+
+        bool Connect();
+
+        void Disconnect();
+
+        void SendData(byte[] data);
+
+        void SetTerminalWindowSize(int columns, int rows, int width, int height);
+    }
+}
---
+++ VtNetCore/VtNetCore/VirtualTerminal/Enums/ECharacterSet.cs
@@ -0,0 +1,21 @@
+﻿namespace VtNetCore.VirtualTerminal.Enums
+{
+    public enum ECharacterSet
+    {
+        C0,
+        C1,
+        C2,
+        UK,
+        USASCII,
+        Dutch,
+        Finnish,
+        French,
+        FrenchCanadian,
+        German,
+        Italian,
+        NorwegianDanish,
+        Spanish,
+        Swedish,
+        Swiss
+    }
+}
