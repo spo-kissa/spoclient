3ece32fe2d1c36ff36a9475ec020ab4e5881ba61
DAISUKE <dai@iil.jp>
2025-03-24 02:51:05 +0900

    Import private keys (#23)
    * 秘密鍵のファイルからのインポート・エクスポートに対応しました
    
    * クリップボード経由の秘密鍵のインポート・エクスポートを実装
    
    ---------
    
    Co-authored-by: Ii Daisuke <dai@e-frontier.systems>

--- spoclient/ViewModels/ServerDialogViewModel.cs
+++ spoclient/ViewModels/ServerDialogViewModel.cs
@@ -1,9 +1,16 @@
-﻿using Prism.Commands;
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using FluentAvalonia.Core;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using spoclient.Extensions;
 using spoclient.Models;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace spoclient.ViewModels
 {
@@ -166,6 +173,196 @@ namespace spoclient.ViewModels
                 this.Title = "Server Edit Entry";
             }
         }
+
+
+        /// <summary>
+        ///     ファイルから秘密鍵をインポートメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ImportPrivateKeyFileCommand => new(async (e) =>
+        {
+            var topLevel = TopLevel.GetTopLevel(e);
+
+            if (topLevel is null)
+            {
+                return;
+            }
+
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open SSH Private Key File",
+                AllowMultiple = false,
+            });
+
+            if (files.Count >= 1)
+            {
+                await using var stream = await files[0].OpenReadAsync();
+                using var streamReader = new StreamReader(stream);
+                var fileContent = await streamReader.ReadToEndAsync();
+
+                if (string.IsNullOrWhiteSpace(fileContent))
+                {
+                    return;
+                }
+
+                PrivateKey = fileContent;
+            }
+        }, (e) =>
+        {
+            return true;
+        });
+
+
+        /// <summary>
+        ///     クリップボードから秘密鍵をインポートメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ImportPrivateKeyClipboadCommand => new(async (e) =>
+        {
+            var topLevel = TopLevel.GetTopLevel(e);
+
+            if (topLevel is null)
+            {
+                return;
+            }
+
+            if (topLevel.Clipboard is null)
+            {
+                return;
+            }
+
+            var formats = await topLevel.Clipboard.GetFormatsAsync();
+
+            if (formats is null)
+            {
+                return;
+            }
+
+
+            foreach (var format in formats)
+            {
+                try
+                {
+                    var data = await topLevel.Clipboard.GetDataAsync(format);
+                    System.Diagnostics.Debug.WriteLine("==================================================================");
+                    System.Diagnostics.Debug.WriteLine(format);
+                    System.Diagnostics.Debug.WriteLine(data);
+
+
+                    if (format == "Files" && data is IEnumerable<IStorageItem> enumerator)
+                    {
+                        foreach (var item in enumerator)
+                        {
+                            System.Diagnostics.Debug.WriteLine(item.Name);
+                        }
+                    }
+                    if (format == "FileName" && data != null)
+                    {
+                        var fileName = Encoding.Default.GetString((byte[])data).Trim();
+                        System.Diagnostics.Debug.WriteLine(fileName);
+                        using var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+                        using var streamReader = new StreamReader(fileStream);
+                        PrivateKey = await streamReader.ReadToEndAsync();
+                    }
+                    if (format == "Text" && data != null)
+                    {
+                        PrivateKey = data?.ToString();
+                    }
+                }
+                catch { }
+            }
+
+        }, (e) =>
+        {
+            var clipboard = TopLevel.GetTopLevel(e)?.Clipboard;
+            if (clipboard is null)
+            {
+                return false;
+            }
+            var formats = clipboard.GetFormatsAsync().Result;
+            return formats.Length > 0;
+        });
+
+
+
+        /// <summary>
+        ///     エクスポートメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ExportCommand => new((e) =>
+        {
+        }, (e) =>
+        {
+            return !string.IsNullOrWhiteSpace(PrivateKey);
+        });
+
+
+
+        /// <summary>
+        ///     ファイルへ秘密鍵をエクスポートメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ExportPrivateKeyFileCommand => new(async (e) =>
+        {
+            var topLevel = TopLevel.GetTopLevel(e);
+
+            if (topLevel is null)
+            {
+                return;
+            }
+
+            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            {
+                Title = "Export SSH Private Key File",
+                DefaultExtension = ".key",
+                ShowOverwritePrompt = true,
+                SuggestedFileName = "private.key",
+            });
+
+            if (file is not null && PrivateKey is not null)
+            {
+                await using var stream = await file.OpenWriteAsync();
+                using var streamWriter = new StreamWriter(stream, Encoding.UTF8);
+                await streamWriter.WriteAsync(PrivateKey);
+            }
+        }, (e) =>
+        {
+            return !string.IsNullOrWhiteSpace(PrivateKey);
+        });
+
+
+        /// <summary>
+        ///     クリップボードへ秘密鍵をエクスポートメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ExportPrivateKeyClipboadCommand => new(async (e) =>
+        {
+            var topLevel = TopLevel.GetTopLevel(e);
+
+            if (topLevel is null)
+            {
+                return;
+            }
+
+            if (topLevel.Clipboard is null)
+            {
+                return;
+            }
+
+            await topLevel.Clipboard.SetTextAsync(PrivateKey);
+
+        }, (e) =>
+        {
+            return !string.IsNullOrWhiteSpace(PrivateKey);
+        });
+
+
+
+        /// <summary>
+        ///     クリアメニューコマンド
+        /// </summary>
+        public DelegateCommand<UserControl> ClearPrivateKeyCommand => new((e) =>
+        {
+            PrivateKey = null;
+        }, (e) =>
+        {
+            return !string.IsNullOrWhiteSpace(PrivateKey);
+        });
 
 
         /// <summary>
