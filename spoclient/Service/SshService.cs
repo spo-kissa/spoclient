e9ca64b08d09e808b703048b218ad26c1e65be65
DAISUKE <dai@iil.jp>
2025-04-17 12:25:05 +0900

    Merge pull request #40 from spo-kissa/change-namespace
    Namespace changes and minor fixes.

--- spoclient/Service/SshService.cs
+++ spoclient/Service/SshService.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -100,7 +99,7 @@ namespace spoclient.Service
             }
             var user = secureServer.User;
 
-            var pass = Marshal.PtrToStringUni(Marshal.SecureStringToGlobalAllocUnicode(secureServer.Password));
+            var pass = secureServer.Password?.ToUnsecureString();
 
             SecureServer = secureServer;
             ConnectionInfo = new PasswordConnectionInfo(host, port, user, pass);
